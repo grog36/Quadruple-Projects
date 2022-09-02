c10aa8a3db8b3016c668aad2676c4bd131cf07d3
grog36 <gregistigger@gmail.com>
2022-09-02 14:16:49 -0400

    Changed Primes.java

--- Primes/Primes.java
+++ Primes/Primes.java
@@ -1,28 +1,29 @@
-public class Primes {
-    public static void main(String[] args) {
-        int num = 100;
-        boolean test = true;
-        String output = "[";  
+import java.lang.Math;
+public class newPrimes {
+	public static void main(String[] args) {
+		int limit = 100000;
+		boolean isPrime = true;
+		String output = "[";
 
-        for (int i=num; i>=2; i--) {
-            for (int t=2; t<i; t++) {
-                if (i%t==0) {
-                    test = false;
-                }
-            }
-            if (test==true) {
-                if (output == "[") {
-                    output += i;
-                }
-                else {
-                    output += ", ";
-                    output += i;
-                }
-            }
-            test = true;
-        }
-
-        output += "]";
-        System.out.println(output);
-    }
+		for (int i = limit; i >= 2; i--) {
+			for (int j = 2; j < Math.floor(Math.sqrt(i)+1); j++) {
+				if (i % j == 0) {
+					isPrime = false;
+				}
+			}
+			if (isPrime) {
+				if (output == "[") {
+					output += i;
+				}
+				else {
+					output += ", " + i;
+				}
+			}
+			else {
+				isPrime = true;
+			}
+		}
+		output += "]";
+		System.out.println(output);
+	}
 }
